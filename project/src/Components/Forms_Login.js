3dc51480d33bfe5c773cd7eed74c1e06828854a2
Rocky Fierro <finnslighthouse@gmail.com>
2020-08-14 02:33:53 +0000

    one functioning form, onward to validation

--- project/src/Components/Forms_Login.js
+++ project/src/Components/Forms_Login.js
@@ -2,7 +2,27 @@ import React, { useState } from "react";
 
 const LoginForm = () => {
 
+    //managing state for INPUTS
+    const [formState, setFormState] = useState({
+        name:"",
+        email:"",
+        password:"",
+        terms:false
+    })
+    //managing state for BUTTONS!!!
+    const [buttonDisabled, setButtonDisabled] = useState(false)
     {/**schema */}
+
+    const inputChange =event=>{
+        const newFormData = {
+            ...formState,
+            [event.target.name]:
+            event.target.type === "checkbox" ? event.target.checked : event.target.value
+       }
+       //validateChange(event)
+       setFormState(newFormData);
+    }
+
 return(
     <div>
         {/*remember to comment!!!*/}
@@ -12,26 +32,26 @@ return(
             <form onSubmit={console.log("submitted!")}>
                 <div>
                     <label htmlFor="name">Name: </label>
-                    <input id="name" type="text" name="name" placeholder="type here please"/>
+                    <input id="name" type="text" name="name" placeholder="type here please" value={formState.name} onChange={inputChange}/>
                 </div>
 
                 <div>
                     <label htmlFor="email">E-Mail: </label>
-                    <input id="email" type="email" name="email" placeholder="please, no hotmail accounts."/>
+                    <input id="email" type="email" name="email" placeholder="please, no hotmail accounts." value={formState.email} onChange={inputChange}/>
                 </div>
 
                 <div>
                     <label htmlFor="password">Password: </label>
-                    <input id="password" type="password" name="password" placeholder="be safe"/>
+                    <input id="password" type="password" name="password" placeholder="be safe" value={formState.password} onChange={inputChange}/>
                 </div>
 
                 <div>
                     <label htmlFor="terms">Please read terms of use before continuing</label>
-                    <input id="terms" type="checkbox" name="terms"/>
+                    <input id="terms" type="checkbox" name="terms" value={formState.terms} onChange={inputChange}/>
                 </div>
 
                 <div>
-                    <button type="submit">Allons-y</button>
+                    <button disabled={buttonDisabled} type="submit">Allons-y</button>
                 </div>
             </form>            
 
